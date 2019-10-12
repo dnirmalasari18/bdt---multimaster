5f17a10e3a5a1e9546922e122443811f706f6b63
dnirmalasari18 <dnirmalasari18@gmail.com>
2019-10-12 19:04:56 +0700

    website

--- config/create_proxysql_user.sql
+++ config/create_proxysql_user.sql
@@ -2,6 +2,6 @@ CREATE USER 'monitor'@'%' IDENTIFIED BY 'monitorpassword';
 GRANT SELECT on sys.* to 'monitor'@'%';
 FLUSH PRIVILEGES;
 
-CREATE USER 'playgrounduser'@'%' IDENTIFIED BY 'playgroundpassword';
-GRANT ALL PRIVILEGES on playground.* to 'playgrounduser'@'%';
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES on lpencerdas.* to 'user'@'%';
 FLUSH PRIVILEGES;
